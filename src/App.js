1344e3e8447aabfdbcd53ec5f84e27d3e69fe350
Basil Jiji <basiljiji2015@gmail.com>
2023-03-25 07:51:35 +0000

    Flipbook Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Flipbook from "./components/Flipbook";
+
+
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Department of MCA</h1>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <h1>Welcome to Department of MCA</h1>
+        <Flipbook />
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
  import React from 'react'
+ import { Container, Row, Navbar, Nav } from 'react-bootstrap'
  
  const Header = () => {
    return (
-     <div>
-       
-     </div>
+     <header>
+       <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+        <Container>
+        <Navbar.Brand href="/">MCA</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#"><i className='fas fa-home'></i> Home</Nav.Link>
+            <Nav.Link href="#"><i className='fas fa-book'></i> Pizada</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+    </Navbar>
+     </header>
    )
  }
  
